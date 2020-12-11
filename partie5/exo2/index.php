04e291918c195a81fe9dc33f8525f4f83ce02106
unknown <isa.fof76@gmail.com>
2020-12-11 20:00:39 +0100

    re-order git repo

---
+++ partie5/exo2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 5 exo 2</title>
+</head>
+<body>
+    <h1> LES TABLEAUX</h1>
+    <p>
+    <!-- Créer un tableau **months** et l'initialiser avec les valeurs suivantes :
+- **janvier**
+- **février**
+- **mars**
+- **avril**
+- **mai**
+- **juin**
+- **juillet**
+- **aout**
+- **septembre**
+- **octobre**
+- **novembre**
+- **décembre**  -->
+Avec le tableau de l'exercice 1, afficher la valeur de la troisième ligne de ce tableau.
+    </p>
+
+    <?php
+    $month = array ('janvier' ,'février' , 'mars' , 'avril','mai' ,'juin' , 'juillet' , 'août','septembre' ,'octobre' , 'novembre' , 'décembre');
+    var_dump ($month [2]);
+
+?>
+    
+</body>
+</html>
