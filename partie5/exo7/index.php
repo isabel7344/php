0bf0e4c46bfa096541b92342ac13cf50e9f6c171
unknown <isa.fof76@gmail.com>
2020-12-13 14:55:02 +0100

    exo7 partie5

---
+++ partie5/exo7/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 5 exo 7</title>
+</head>
+<body>
+    <h1> LES TABLEAUX</h1>
+    <p>Avec le tableau de l'exercice 5, ajouter la ligne correspondant au département de la ville de Reims.
+</p>
+ 
+
+    <?php
+    $départementsHautDeFrance[02] = 'Aisne';
+    $départementsHautDeFrance[59] = 'Nord';
+    $départementsHautDeFrance[60] = 'Oise';
+    $départementsHautDeFrance[62] = 'Pas-de-Calais';
+    $départementsHautDeFrance[80] = 'Somme';
+    $départementsHautDeFrance[51] = 'Reims';
+    var_dump($départementsHautDeFrance);
+?>
+    
+</body>
+</html>
