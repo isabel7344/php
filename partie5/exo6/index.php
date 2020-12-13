abd8e1acc3b3faf52632966f36fbe418f096493e
unknown <isa.fof76@gmail.com>
2020-12-13 14:42:43 +0100

    exo6 partie5

---
+++ partie5/exo6/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 5 exo 6</title>
+</head>
+<body>
+    <h1> LES TABLEAUX</h1>
+    <p>Avec le tableau de l'exercice 5, afficher la valeur de l'index 59.
+</p>
+ 
+
+    <?php
+    $départementsHautDeFrance[02] = 'Aisne';
+    $départementsHautDeFrance[59] = 'Nord';
+    $départementsHautDeFrance[60] = 'Oise';
+    $départementsHautDeFrance[62] = 'Pas-de-Calais';
+    $départementsHautDeFrance[80] = 'Somme';
+    var_dump($départementsHautDeFrance[59]);
+?>
+    
+</body>
+</html>
