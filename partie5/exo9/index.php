42b6c774959cd36486c686abd1348b59b5e6d1f0
unknown <isa.fof76@gmail.com>
2020-12-13 15:55:15 +0100

    modif exo9 partie5

--- partie5/exo9/index.php
+++ partie5/exo9/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>partie 5 exo 8</title>
+    <title>partie 5 exo 9</title>
 </head>
 <body>
     <h1> LES TABLEAUX</h1>
