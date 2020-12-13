3c13acbc6526bb082c88f5a52d1c9c88a5506eff
unknown <isa.fof76@gmail.com>
2020-12-13 13:57:04 +0100

    exo3 partie5

---
+++ partie5/exo3/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 5 exo 2</title>
+</head>
+<body>
+    <h1> LES TABLEAUX</h1>
+    <p>
+    <!-- Créer un tableau **months** et l'initialiser avec les valeurs suivantes :
+- **janvier**
+- **février**
+- **mars**
+- **avril**
+- **mai**
+- **juin**
+- **juillet**
+- **aout**
+- **septembre**
+- **octobre**
+- **novembre**
+- **décembre**  -->
+Avec le tableau de l'exercice , afficher la valeur de l'index 5.
+    </p>
+
+    <?php
+    $month = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+    var_dump($month[4]);
+
+?>
+    
+</body>
+</html>
