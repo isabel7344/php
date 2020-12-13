070aa7088e988b6c15f47bfd73eddf4623af2db7
unknown <isa.fof76@gmail.com>
2020-12-13 14:37:55 +0100

    exo5

---
+++ partie5/exo5/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 5 exo 5</title>
+</head>
+<body>
+    <h1> LES TABLEAUX</h1>
+    <p>Créer un tableau associatif avec comme index le numéro des départements des Hauts de France et en valeur leur nom.
+</p>
+ 
+
+    <?php
+    $départementsHautDeFrance[02] = 'Aisne';
+    $départementsHautDeFrance[59] = 'Nord';
+    $départementsHautDeFrance[60] = 'Oise';
+    $départementsHautDeFrance[62] = 'Pas-de-Calais';
+    $départementsHautDeFrance[80] = 'Somme';
+    var_dump($départementsHautDeFrance);
+?>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
