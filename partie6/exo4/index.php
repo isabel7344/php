4f8730a410a6971c7acb753f5a86351d78863cb9
unknown <isa.fof76@gmail.com>
2020-12-13 17:10:17 +0100

    exo4 partie6

---
+++ partie6/exo4/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 6 exo 4</title>
+</head>
+
+<body>
+    <h1> Les paramètres d'URL</h1>
+    <p>
+    Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent 
+     et les afficher: **index.php?language=PHP&server=LAMP**
+    </p>
+
+  
+    <?php
+// Détermine si  tous les paramètres d\'une url existent
+  $url = 'https://exos-php/partie6/exo1/';
+  if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
+      echo('URL est valide .<br>. index.php?language=PHP&server=LAMP ');
+  } else {
+      echo("URL n'est pas valide");
+  }
+?>
+
+
+</body>
+
+</html>
