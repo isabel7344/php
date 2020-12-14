41b8c126141dd40cd6bb7911f3a817a5a30ccd91
unknown <isa.fof76@gmail.com>
2020-12-14 17:29:44 +0100

    correction exo1 partie6bis

--- partie6bis/exo1/index.php
+++ partie6bis/exo1/index.php
@@ -1,8 +1,12 @@
 <?php
 // $_GET affiche le tableau et $_POST l'affiche que dans le serveur
-
+$lastname = 'Param non présent ';
+$firstname = 'Param non présent ';
 if (isset($_GET['lastname']) && isset($_GET['firstname'])) {
-    echo $_GET['lastname'] . ' ' . $_GET['firstname'];
+    $lastname = $_GET['lastname'];
+    $firstname = $_GET['firstname'];
+
+    // echo $_GET['lastname'] . ' ' . $_GET['firstname'];
 
     // var_dump($_GET['lastname']) ;
     // var_dump($_GET['firstname']) ;
@@ -22,26 +26,37 @@ if (isset($_GET['lastname']) && isset($_GET['firstname'])) {
 
 <body>
 
-   
 
-<form>
-    <a class="btn btn-primary" href="index.php?lastname=Tutor&amp;firstname=Janine">Envoie des paramètre</a>
-    <a class="btn btn-secondary" href="index.php?">Retour Index</a>
-</form> 
-<h1> Les paramètres d'URL</h1>
-<p>
-        Voici l'URL à étudier :
-        index.php?lastname=Tutor&firstname=Janine
 
-        Faire une page index.php.
-        Sur cette page faire 2 boutons :
+    <div class="container">
+        <div class="d-flex justify-content-center m-5">
+            <a class="btn btn-primary" href="index.php?lastname=Tutor&amp;firstname=Janine">Envoie des paramètre</a>
+            <a class="btn btn-secondary" href="index.php">Retour Index</a>
+        </div>
 
-        Le premier "bouton" doit contenir un href qui pointe vers l'URL.
-        Il faut ensuite tester sur cette même page que tous les paramètres existent :
-        S'ils sont présents les afficher.
-        Dans le cas contraire ne rien afficher.
-        Le deuxieme "bouton" doit permettre de revenir à la page index.php.
-    </p>
+        <p class="text-center"> Param1 : <?= $lastname ?>
+        </p>
+        <p class="text-center"> Param2 : <?= $firstname ?>
+        </p>
+    </div>
+
+
+    <div class="Consigne text-center ">
+        <h1> Les paramètres d'URL</h1>
+        <p>
+            Voici l'URL à étudier :
+            index.php?lastname=Tutor&firstname=Janine
+
+            Faire une page index.php.
+            Sur cette page faire 2 boutons :
+
+            Le premier "bouton" doit contenir un href qui pointe vers l'URL.
+            Il faut ensuite tester sur cette même page que tous les paramètres existent :
+            S'ils sont présents les afficher.
+            Dans le cas contraire ne rien afficher.
+            Le deuxieme "bouton" doit permettre de revenir à la page index.php.
+        </p>
+    </div>
 
 </body>
 
