3eac208a5a0124d7f02969dc4e0bb8aa76f0a7bb
unknown <isa.fof76@gmail.com>
2020-12-23 10:07:05 +0100

    exo7 partie9

---
+++ partie9/exo7/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Partie 9 exo5</title>
+</head>
+
+<body>
+<h1> Les Dates</h1>
+    <p>
+    Afficher la date du jour + 20 jours.
+    </p>
+     <?php
+
+    $date = strftime('%d-%m-%Y', mktime(0, 0, 0, date('m'), date('d') + 20, date('y')));
+    echo $date;
+
+    ?>
+</body>
+
+</html>
